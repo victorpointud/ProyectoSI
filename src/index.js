16e995d43ad8a8cfc69ef79e4d3609704e731095
victorpointud <113309762+victorpointud@users.noreply.github.com>
2024-06-21 15:32:56 -0400

    1

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './components/styles/index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
