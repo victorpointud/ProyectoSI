549916eb1b3b2e4a6881e809a524949b366fd3c4
victorpointud <113309762+victorpointud@users.noreply.github.com>
2024-06-24 18:54:57 -0400

    1

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export const Header = (props) => {
+  const scrollToSection = (id) => {
+    const element = document.getElementById(id);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
+  };
+
   return (
     <header id="header">
       <div className="intro">
@@ -8,17 +15,15 @@ export const Header = (props) => {
           <div className="container">
             <div className="row">
               <div className="col-md-8 col-md-offset-2 intro-text">
-                <h1>
-                  {props.data ? props.data.title : "Loading"}
-                  <span></span>
-                </h1>
+                <h1>{props.data ? props.data.title : "Loading"}</h1>
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
-                  href="#features"
+                  href="#about"
                   className="btn btn-custom btn-lg page-scroll"
+                  onClick={() => scrollToSection('about')}
                 >
                   Más Sobre Nosotros
-                </a>{" "}
+                </a>
               </div>
             </div>
           </div>
